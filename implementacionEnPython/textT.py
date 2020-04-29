577d258b4d8ad90645e621e19dc084d6d5759682
Nicolas Arzani Domenget <arzanico@hotmail.com>
2020-04-29 19:41:29 +0800

    Create textT.py
    Funcion de transformacion de texto

---
+++ implementacionEnPython/textT.py
@@ -0,0 +1,35 @@
+# La funcion tiene como objetivo limpiar algunas cuestiones del texto.
+# pero tambien genera el formato de nombre "Camello"
+# si hubiese una convencion para los nombre, de tablas, variables, etc. Aqui se puede incluir el formato
+# Otro tema importante es el enconding del texto, aqui para generalizar esta en utf-8 pero sin acentos y si la virgulilla de la ñ
+
+def transform(word=None, upFLetter=None, isCamel=None):
+    if not isCamel:
+        word = word.lower().strip()
+    else:
+        word = word.strip()
+
+    word = word.replace('.', '')
+    intab = "áéíóúñÁÉÍÓÚÑ"
+    outtab = "aeiounAEIOUN"
+    trantab = str.maketrans(intab, outtab)
+    word = word.translate(trantab)
+    
+    # Convierto a camel Tiping si es posible
+    if len(word.split()):
+        compWord = word.split()
+        if upFLetter:
+            newCompWord = [compWord[0].capitalize()]
+        else:
+            newCompWord = [compWord[0]]
+        for w in compWord[1:]:
+            w = w.capitalize()
+            newCompWord.append(w)
+        word = ''.join([x for x in newCompWord])
+    # Si upFLetter es True, convierto la primera letra a mayuscula
+    elif upFLetter:
+        wL = [x for x in word]
+        wL[0] = wL[0].upper()
+        word = ''.join([l for l in wL])
+
+    return word
